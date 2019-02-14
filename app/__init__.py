bf1eeb26718d41f551f819c0a639560c94f10bd1
yangchen29 <44721777+yangchen29@users.noreply.github.com>
2019-02-15 02:36:39 +0800

    correct mistakes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
-from app.exts import auth
+# encoding: utf-8
+
+from flask import Flask
+from app import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPTokenAuth, HTTPBasicAuth
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_object(config)
+auth = HTTPBasicAuth()
+
 from app.models import User
+from flask import g
+
 
 @auth.verify_password
 def verify_password(username, password):
-    user = User.query.filter_by(username = username).first()
+    user = User.query.filter_by(username=username).first()
     if not user or not user.verify_password(password):
         return False
     g.user = user
     return True
-
